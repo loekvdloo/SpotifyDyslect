dc23192b757a815bc8cd13f167fd3888ff73173b
loekvandeloo <1206994@student.roc-nijmegen.nl>
2025-05-19 11:15:07 +0200

    keuzes toegevoegd van begin scherm

--- SpotifyDyslect/Program.cs
+++ SpotifyDyslect/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("aoeifj");
+﻿using System;
+
+namespace SpotifyDyslect
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welkom bij SpotifyDyslect");
+            Console.WriteLine("Maak uw keuze");
+            Console.WriteLine("1. Bekijk albums");
+            Console.WriteLine("2. Maak playlist");
+            Console.WriteLine("3. Bekijk playlisten");
+            Console.WriteLine("4. Bekijk vrienden");
+            Console.WriteLine("5. Vergelijk playlisten met vrienden");
+            Console.WriteLine("6. afsluiten");
+
+
+
+        }
+    }
+}
